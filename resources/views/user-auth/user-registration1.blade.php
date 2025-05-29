13d636af44328c3a80c6c688c6c3f10ed417abec
Psyiantist <castaneda.mattg9@gmail.com>
2025-05-29 22:44:09 +0800

    Add Font Awesome 5.15.1 stylesheet to user registration view

--- resources/views/user-auth/user-registration1.blade.php
+++ resources/views/user-auth/user-registration1.blade.php
@@ -7,6 +7,7 @@
     <title>Registration - Step 1</title>
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"/>
     <style>
         html, body {
             margin: 0;
