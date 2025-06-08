2690e4583e214d96a7f376300488226aba18b42b
Psyiantist <castaneda.mattg9@gmail.com>
2025-06-09 00:21:19 +0800

    refactor: Remove unnecessary verification check for pending users in admin view, streamlining user retrieval process

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -21,8 +21,7 @@ class EmployeeController extends Controller
 
     public function getPendingUsers()
     {
-        $pending_users = User::where('is_verified', '0')
-            ->where('status', 'Pending')
+        $pending_users = User::where('status', 'Pending')
             ->select('id', 'first_name', 'last_name', 'email', 'id_type', 'id_image', 'current_address', 'date_of_birth', 'status')
             ->get();
             
